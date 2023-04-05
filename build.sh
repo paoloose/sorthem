a7aa7f0705707ae5e5749bbbf8c4d1a4ab007339
Paolo Flores <pflores.fisi22@gmail.com>
2023-04-05 12:03:13 -0500

    build: implement building for windows using git bash

--- build.sh
+++ build.sh
@@ -2,9 +2,19 @@
 
 compiler="g++"
 flags="-Wall -Wextra -pedantic"
-libs="-lsfml-graphics -lsfml-window -lsfml-system"
 bin_name="sorthem"
 build_dir="./dist"
+libs="-lsfml-graphics -lsfml-window -lsfml-system"
+# if you installed SFML in a non standard path
+linker_dir=""
+include=""
+
+case "$(uname -s)" in MINGW*)
+    # for windows
+    SFML_path='C:\SFML-2.5.1'
+    include="-I$SFML_path\include"
+    linker_dir="-L$SFML_path\lib"
+esac
 
 set -e
 set -x
@@ -15,11 +25,11 @@ mkdir -p $build_dir
 
 for unit in $units; do
     dest=$build_dir/$(basename $unit .cpp).o
-    $compiler -c $unit -o $dest $flags
+    $compiler -c $unit -o $dest $flags $include
 done
 
 objs=$(find $build_dir -name "*.o")
-$compiler $objs -o $build_dir/$bin_name $flags $libs
+$compiler $linker_dir $objs -o $build_dir/$bin_name $flags $libs
 
 if [ "$1" = "run" ]; then
     shift 1
